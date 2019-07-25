5512864440f55da2d946ca87f2ab3724e6d462e5
ElderGr <gbielrs@gmail.com>
2019-07-24 22:51:52 -0300

    realizado segunda bateria de testes nas classes criadas

--- spec/ConsultaSpec.js
+++ spec/ConsultaSpec.js
@@ -1,23 +1,33 @@
 describe("Consulta", function(){
 
-    it("não deve cobrar nada se for um retorno", function(){
-        var guilherme = new Paciente("Guilherme", 28, 1.72, 1.80);
-        var consulta = new Consulta(guilherme, [], true, true);
+    var guilherme;
 
-        expect(consulta.preco()).toEqual(0);
+    //códigos ou blocos repetidos
+    beforeEach(function(){
+        guilherme = new PacienteBuilder().constroi();
+    });
+    
+    //descrive em um nível menor, que agrupa os testes baseado na necessidade
+    //no log esses testes são exibidos tabulados também, o que facilita
+    describe("consulta do tipo retorno", function(){
+        it("não deve cobrar nada se for um retorno", function(){
+            var consulta = new Consulta(guilherme, [], true, true);
+    
+            expect(consulta.preco()).toEqual(0);
+        });
     });
 
-    it("deve cobrar 25 por cada procedimento comum", function(){
-        var guilherme = new Paciente("Guilherme", 28, 1.72, 1.80);
-        var consulta = new Consulta(guilherme, ['proc1','proc2'], false, false);
-
-        expect(consulta.preco()).toEqual(50);
-    });
-
-    it("deve cobrar preco especifico dependendo do procedimento", function(){
-        var guilherme = new Paciente("Guilherme", 28, 1.72, 1.80);
-        var consulta = new Consulta(guilherme, ['procedimento-comum', "raio-x", "procedimento-comum","gesso"], false, false);
-
-        expect(consulta.preco()).toEqual(25 + 55 + 25 + 32);
+    describe("consultas com procedimentos", function(){
+        it("deve cobrar 25 por cada procedimento comum", function(){
+            var consulta = new Consulta(guilherme, ['proc1','proc2'], false, false);
+    
+            expect(consulta.preco()).toEqual(50);
+        });
+    
+        it("deve cobrar preco especifico dependendo do procedimento", function(){
+            var consulta = new Consulta(guilherme, ['procedimento-comum', "raio-x", "procedimento-comum","gesso"], false, false);
+    
+            expect(consulta.preco()).toEqual(25 + 55 + 25 + 32);
+        });
     });
 })
